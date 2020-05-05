fa91a1d194e9151ab05cca3155d5bdd159f819c0
Travis <travis@denouncetheclock.com>
2020-05-05 20:51:16 +1000

    Clock now shows jpn time text that matches face

--- sketch.js
+++ sketch.js
@@ -1,17 +1,35 @@
-const CANVAS_WIDTH = 300;
-const CANVAS_HEIGHT = 300;
+const CANVAS_WIDTH = window.innerWidth > 500 ? 500 : window.innerWidth;
+const CANVAS_HEIGHT = CANVAS_WIDTH;
 const CLOCKSIZE = CANVAS_WIDTH - 50;
 const CLOCK_NUMBERS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 const MINUTE_HAND_LENGTH = CLOCKSIZE / 2 - 30;
 const HOUR_HAND_LENGTH = CLOCKSIZE / 2 - 50;
+const TIME_DIV_ENG = document.getElementById('time-eng');
+const TIME_DIV_JPN = document.getElementById('time-jpn');
+
+let timeHr = 0;
+let timeMin = 0;
 
 function setup() {
   createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
   background(255, 0, 0);
+  generateTime();
+  showJapaneseIime();
   drawClock();
 }
 
-function draw() {
+function generateTime() {
+  timeHr = Math.floor(random(0, 12));
+  timeMin = Math.floor(random(0, 59));
+  let timeMinPretty = timeMin < 10 ? `0${timeMin}` : timeMin;
+  console.log(timeMinPretty);
+  TIME_DIV_ENG.innerText = `${timeHr}:${timeMinPretty}`;
+}
+
+function showJapaneseIime() {
+  const hr = HOURS_JPN[timeHr];
+  const min = MINS_JPN[timeMin];
+  TIME_DIV_JPN.innerText = `${hr} ${min}`;
 }
 
 function drawClock() {
@@ -19,8 +37,8 @@ function drawClock() {
   ellipse(width/2, height/2, CLOCKSIZE);
   drawNumbers();
   drawMinuteLines();
-  drawMinuteHand();
-  drawHourHand();
+  drawMinuteHand(timeMin);
+  drawHourHand(timeHr, timeMin);
 }
 
 function drawNumbers() {
@@ -34,49 +52,30 @@ function drawNumbers() {
   let time = Math.PI - timeAdd; // Start at right spot
   CLOCK_NUMBERS.forEach(number => {
     text(number, sin(time)*multX+offsetX, cos(time)*multY+offsetY);
-    // ellipse(, 20);
     time-= timeAdd;
   });
 }
 
-function drawMinuteHand() {
+function drawMinuteHand(minute) {
   strokeWeight(2);
   let offsetX = width / 2;
   let offsetY = height / 2;
-  let multX = CLOCKSIZE / 2;
-  let multY = CLOCKSIZE / 2;
-  let tickLength = MINUTE_HAND_LENGTH;
-  let timeAdd = Math.PI * 2 / 60; // 60 ticks
-  let time = 0;
-  const r = Math.floor(random(0, 61));
-  for (let index = 0; index < 61; index++) {
-    if (r == index) {
-      console.log('11111111111111');
-      let s = sin(time);
-      let c = cos(time);
-      line(offsetX, offsetY, s*tickLength+offsetX, c*tickLength+offsetY); // s*(multX-tickLength)+offsetX, c*(multY-tickLength)+offsetY);
-      break;
-    }
-    time-=timeAdd;
-  }
+  let time = Math.PI - ((Math.PI * 2) / 60) * minute;
+  let s = sin(time);
+  let c = cos(time);
+  line(offsetX, offsetY, s*MINUTE_HAND_LENGTH+offsetX, c*MINUTE_HAND_LENGTH+offsetY);
 }
 
-function drawHourHand() {
+function drawHourHand(hour, minute) {
   strokeWeight(4);
   let offsetX = width / 2;
   let offsetY = height / 2;
-  let timeAdd = Math.PI * 2 / CLOCK_NUMBERS.length; // 60 ticks
-  let time = 0;
-  const r = Math.floor(random(0, CLOCK_NUMBERS.length));
-  for (let index = 0; index < CLOCK_NUMBERS.length; index++) {
-    if (r == index) {
-      let s = sin(time);
-      let c = cos(time);
-      line(offsetX, offsetY, s*HOUR_HAND_LENGTH+offsetX, c*HOUR_HAND_LENGTH+offsetY); // s*(multX-tickLength)+offsetX, c*(multY-tickLength)+offsetY);
-      break;
-    }
-    time-=timeAdd;
-  }
+  let time = Math.PI - ((Math.PI * 2) / 12) * hour;
+  let minOffset = ((Math.PI * 2) / 12) * (minute / 60);
+  time -= minOffset;
+  let s = sin(time);
+  let c = cos(time);
+  line(offsetX, offsetY, s*HOUR_HAND_LENGTH+offsetX, c*HOUR_HAND_LENGTH+offsetY);
 }
 
 function drawMinuteLines() {
@@ -89,6 +88,7 @@ function drawMinuteLines() {
   let time = 0;
   for (let index = 0; index < 61; index++) {
     // TODO: Modulo this to not draw those div by 5
+    // if (index % 5 === 0) continue;
     let s = sin(time);
     let c = cos(time);
     line(s*multX+offsetX, c*multY+offsetY, s*(multX-tickLength)+offsetX, c*(multY-tickLength)+offsetY);
